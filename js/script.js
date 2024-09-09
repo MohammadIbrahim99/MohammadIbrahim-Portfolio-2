6f52975343c92fe79d456a8b577cf95ffa3277fa
BaimYow <baimyow1999@gmail.com>
2024-09-09 16:44:57 +0700

    js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 // togle class aktif untuk hamburger
-const hamburger = document.querySelector(".nav");
+const nav = document.querySelector(".nav");
 //ketika hamburger di kilik
 document.querySelector(".hamburger").onclick = (e) => {
-  hamburger.classList.toggle("active");
+  nav.classList.toggle("active");
   e.preventDefault();
 };
+
+//klik diluar hamburger untuk menghilangkan nav
+
+const hm = document.querySelector(".hamburger");
+const menu = document.querySelector("#navbar");
+
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !nav.contains(e.target)) {
+    nav.classList.remove("active");
+  }
+
+  if (menu.contains(e.target)) {
+    nav.classList.remove("active");
+  }
+});
