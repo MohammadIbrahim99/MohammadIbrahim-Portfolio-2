d599db86ebe8b169d10f9141084b05e452173fdd
BaimYow <baimyow1999@gmail.com>
2024-09-12 18:48:07 +0700

    hamburger memu

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
 // togle class aktif untuk hamburger
+const hamMenu = document.querySelector(".hamburger-menu");
 const nav = document.querySelector(".nav");
 //ketika hamburger di kilik
-document.querySelector(".hamburger").onclick = (e) => {
+document.querySelector(".hamburger-menu").onclick = (e) => {
   nav.classList.toggle("active");
+  hamMenu.classList.toggle("active");
   e.preventDefault();
 };
 
 //klik diluar hamburger untuk menghilangkan nav
 
-const hm = document.querySelector(".hamburger");
-const menu = document.querySelector("#navbar");
+// const hm = document.querySelector(".hamburger-menu");
+// const menu = document.querySelector("#navbar");
 
-document.addEventListener("click", function (e) {
-  if (!hm.contains(e.target) && !nav.contains(e.target)) {
-    nav.classList.remove("active");
-  }
+// document.addEventListener("click", function (e) {
+//   if (!hm.contains(e.target) && !nav.contains(e.target)) {
+//     nav.classList.remove("active");
+//   }
 
-  if (menu.contains(e.target)) {
-    nav.classList.remove("active");
-  }
-});
+//   if (menu.contains(e.target)) {
+//     nav.classList.remove("active");
+//   }
+// });
